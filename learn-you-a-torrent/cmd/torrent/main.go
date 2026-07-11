5b10186736ffb363d95767a0106217d05ce4db34
Terence Schumacher <terenceschumacher@gmail.com>
2026-07-10 21:45:45 -0700

    feat(06-02): add SIGINT handling and shutdown progress message

--- learn-you-a-torrent/cmd/torrent/main.go
+++ learn-you-a-torrent/cmd/torrent/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"io"
 	"os"
+	"os/signal"
 	"path/filepath"
+	"syscall"
 
 	"github.com/tazzledazzle/go-cook/learn-you-a-torrent/internal/downloader"
 	"github.com/tazzledazzle/go-cook/learn-you-a-torrent/internal/torrent"
@@ -20,6 +24,24 @@ func main() {
 
 // Run executes CLI commands from args.
 func Run(args []string) error {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	sigCh := make(chan os.Signal, 1)
+	signalNotify(sigCh)
+	go func() {
+		<-sigCh
+		cancel()
+	}()
+
+	return runDownload(ctx, args, os.Stdout)
+}
+
+var signalNotify = func(c chan<- os.Signal) {
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+}
+
+func runDownload(ctx context.Context, args []string, out io.Writer) error {
 	if len(args) < 2 || args[0] != "download" {
 		return fmt.Errorf("usage: torrent download <file.torrent>")
 	}
@@ -38,6 +60,7 @@ func Run(args []string) error {
 		return fmt.Errorf("get working directory: %w", err)
 	}
 
+	var lastProgress torrent.Progress
 	d := &downloader.Downloader{
 		PeerID: peerID,
 		ListPeers: func(t *torrent.Torrent) ([]downloader.PeerAddress, error) {
@@ -52,13 +75,23 @@ func Run(args []string) error {
 			return addrs, nil
 		},
 		OnProgress: func(p torrent.Progress) {
-			fmt.Printf("\r%s", p.String())
+			lastProgress = p
+			fmt.Fprintf(out, "\r%s", p.String())
 		},
 	}
 
-	if err := d.Download(context.Background(), tor, dir); err != nil {
+	downloadErr := d.Download(ctx, tor, dir)
+	return handleDownloadResult(downloadErr, lastProgress, out, tor.Info.Name, dir)
+}
+
+func handleDownloadResult(err error, last torrent.Progress, out io.Writer, name, dir string) error {
+	if errors.Is(err, context.Canceled) {
+		fmt.Fprintf(out, "\nShutdown: %s\n", last.String())
+		return nil
+	}
+	if err != nil {
 		return err
 	}
-	fmt.Printf("\nDownloaded %s to %s\n", tor.Info.Name, filepath.Join(dir, tor.Info.Name))
+	fmt.Fprintf(out, "\nDownloaded %s to %s\n", name, filepath.Join(dir, name))
 	return nil
 }
