d29787d3283aa480a5174dd3fac07fec35991005
Terence Schumacher <terenceschumacher@gmail.com>
2026-07-15 14:23:32 -0700

    maximum number of vowels ina substring of given length

--- coding-problems/sliding-window/main.go
+++ coding-problems/sliding-window/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "strings"
+
 // maximum average subarray
 func findMaxAverage(nums []int, k int) float64 {
 	sum := 0
@@ -14,4 +16,31 @@ func findMaxAverage(nums []int, k int) float64 {
 		}
 	}
 	return float64(best) / float64(k)
+}
+
+// max num of vowels ina substring of given length
+func maxVowels(s string, k int) int {
+	isVowel := func(b byte) bool {
+		return strings.ContainsRune("aeiou", rune(b))
+	}
+	count := 0
+	for i := 0; i < k; i++ {
+		if isVowel(s[i]) {
+			count++
+		}
+	}
+	best := count
+	for i := k; i < len(s); i++ {
+		if isVowel(s[i]) {
+			count++
+		}
+		if isVowel(s[i-k]) {
+			count--
+		}
+		if count > best {
+			best = count
+		}
+	}
+
+	return best
 }
