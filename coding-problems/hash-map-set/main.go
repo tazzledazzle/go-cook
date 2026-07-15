11746687daf0c3043ec10ab625d973d94a90f541
Terence Schumacher <terenceschumacher@gmail.com>
2026-07-15 14:54:39 -0700

    determine if two strings are close

--- coding-problems/hash-map-set/main.go
+++ coding-problems/hash-map-set/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"reflect"
+	"sort"
+)
+
 // difference between two arrays
 func findDifference(nums1 []int, nums2 []int) [][]int {
 	s1, s2 := map[int]bool{}, map[int]bool{}
@@ -38,4 +43,28 @@ func uniqueOccurrences(arr []int) bool {
 		seen[num] = true
 	}
 	return true
+}
+
+// determine if two strings are close
+func closeStrings(word1 string, word2 string) bool {
+	if len(word1) != len(word2) {
+		return false
+	}
+	freq1, freq2 := [26]int{}, [26]int{}
+	for _, ch := range word1 {
+		freq1[ch-'a']++
+	}
+	for _, ch := range word2 {
+		freq2[ch-'a']++
+	}
+	for i := 0; i < 26; i++ {
+		if (freq1[i] == 0) != (freq2[i] == 0) {
+			return false
+		}
+	}
+	ch1 := append([]int{}, freq1[:]...)
+	ch2 := append([]int{}, freq2[:]...)
+	sort.Ints(ch1)
+	sort.Ints(ch2)
+	return reflect.DeepEqual(ch1, ch2)
 }
