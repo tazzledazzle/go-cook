e3fc5b281c89b5a93d0beff475a80e093c3c312d
Terence Schumacher <terenceschumacher@gmail.com>
2026-07-15 14:38:43 -0700

    Find pivot index

--- coding-problems/prefix-sum/main.go
+++ coding-problems/prefix-sum/main.go
@@ -11,3 +11,19 @@ func largestAltitude(gain []int) int {
 	}
 	return best
 }
+
+// find pivot index
+func pivotIndex(nums []int) int {
+	total := 0
+	for _, num := range nums {
+		total += num
+	}
+	leftSum := 0
+	for i, num := range nums {
+		if leftSum == total-leftSum-num {
+			return i
+		}
+		leftSum += num
+	}
+	return -1
+}
