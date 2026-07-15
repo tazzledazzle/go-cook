7e90a819cc628364b1487f74fd0b187393dc47e5
Terence Schumacher <terenceschumacher@gmail.com>
2026-07-15 15:36:26 -0700

    leet(linlis): odd even linked list

--- coding-problems/linked-lists/main.go
+++ coding-problems/linked-lists/main.go
@@ -21,3 +21,20 @@ func deleteMiddle(head *ListNode) *ListNode {
 	prev.Next = slow.Next
 	return head
 }
+
+// odd even linked list
+func oddEvenList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	odd, even := head, head.Next
+	evenHead := even
+	for even != nil && even.Next != nil {
+		odd.Next = even.Next
+		odd = odd.Next
+		even.Next = odd.Next
+		even = even.Next
+	}
+	odd.Next = evenHead
+	return head
+}
