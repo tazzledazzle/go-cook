9248610dbabed8dddd280effd8ba6e305c72e1a7
Terence Schumacher <terenceschumacher@gmail.com>
2026-07-27 11:11:13 -0700

    leet(bstdfs): Merge K sorted lists

--- coding-problems/linked-lists/main.go
+++ coding-problems/linked-lists/main.go
@@ -78,3 +78,53 @@ func pairSum(head *ListNode) int {
 	}
 	return best
 }
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeKLists(lists []*ListNode) *ListNode {
+	/*
+
+	   if the list is empty, return nil
+	   if the list has one, return it
+	   split the list and merge it
+
+	   merge two lists
+	       if either list is empty, return the other
+	       if the left value is less, set the Next to merge two lists with left.Next and right
+	       right.Next set to merge two lists of left and right.Next
+	*/
+	length := len(lists)
+	if length < 1 {
+		return nil
+	}
+	if length == 1 {
+		return lists[0]
+	}
+
+	med := length / 2
+	left := mergeKLists(lists[:med])
+	right := mergeKLists(lists[med:])
+	return recursiveMergeTwoLists(left, right)
+}
+
+func recursiveMergeTwoLists(left *ListNode, right *ListNode) *ListNode {
+	if left == nil {
+		return right
+	}
+	if right == nil {
+		return left
+	}
+
+	if left.Val < right.Val {
+		left.Next = recursiveMergeTwoLists(left.Next, right)
+		return left
+	}
+
+	right.Next = recursiveMergeTwoLists(left, right.Next)
+	return right
+}
