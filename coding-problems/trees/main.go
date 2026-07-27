080ab0ebb4cd27a2cfa66db085b85f72a71a2459
Terence Schumacher <terenceschumacher@gmail.com>
2026-07-27 11:50:34 -0700

    leet(tree): preorder tree

--- coding-problems/trees/main.go
+++ coding-problems/trees/main.go
@@ -1,7 +1,38 @@
 package main
 
+import "fmt"
+
 type BinaryTreeNode struct {
 	left  *BinaryTreeNode
 	data  int
 	right *BinaryTreeNode
+}
+
+// PreOrder
+func PreOrder(root *BinaryTreeNode) {
+	if root == nil {
+		return
+	}
+	fmt.Printf("%d ", root.data)
+	PreOrder(root.left)
+	PreOrder(root.right)
+}
+
+func PreOrderWalk(root *BinaryTreeNode, ch chan int) {
+	if root == nil {
+		return
+	}
+
+	ch <- root.data
+	PreOrderWalk(root.left, ch)
+	PreOrderWalk(root.right, ch)
+}
+
+func PreOrderWalker(root *BinaryTreeNode) <-chan int {
+	ch := make(chan int)
+	go func() {
+		PreOrderWalk(root, ch)
+		close(ch)
+	}()
+	return ch
 }
